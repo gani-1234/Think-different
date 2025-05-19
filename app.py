69bb4ffb62bb43182a16a99948a67157d1b33aaa
ganesh <143176765+gani-1234@users.noreply.github.com>
2025-05-19 18:33:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 model=genai.GenerativeModel("gemini-1.5-flash")
 generate_config=GenerationConfig(temperature=0.4)
 
